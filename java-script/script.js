795ce9a6046b8b4bbb7c10eda93af720c9f79902
Kenta Bilotta <kentabilotta20@gmail.com>
2022-10-24 16:58:57 +0200

    parte interattiva (completato)

--- java-script/script.js
+++ java-script/script.js
@@ -1,4 +1,7 @@
 let btnGenera = document.querySelector(".generate");
+let btnAnnulla = document.querySelector(".cancel");
+let ticket = document.querySelector(".your-ticket");
+
 
 btnGenera.addEventListener("click", function() {
     let nameSurname = document.getElementById("name-surname").value;
@@ -12,27 +15,39 @@ btnGenera.addEventListener("click", function() {
     let minorenne = document.querySelector(".minorenne").value;
     let over65 = document.querySelector(".over-65").value;
 
-    if (age = maggiorenne) {
+    let total = distance * 0.21;
+    let sconto20 = total - ((total * 20) / 100);
+    let sconto40 = total - ((total * 40) / 100);
+
+    if (age === maggiorenne) {
         console.log("Sei maggiorenne");
+        document.querySelector(".prezzo-finale").innerHTML = total + "&euro;";
+        document.querySelector(".ticket-type").innerHTML = "Biglietto Standard";
     } else {
-        if (age = minorenne) {
+        if (age === minorenne) {
             console.log("Sei minorenne");
+            document.querySelector(".prezzo-finale").innerHTML = sconto20 + "&euro;";
+            document.querySelector(".ticket-type").innerHTML = "Biglietto Minore";
         } else {
-            if (age = over65) {
-                console.log("Sei un over65");
+            if (age === over65) {
+                console.log("Sei un over 65");
+                document.querySelector(".prezzo-finale").innerHTML = sconto40 + "&euro;";
+                document.querySelector(".ticket-type").innerHTML = "Biglietto Over";
             }
         }
     }
 
-    let ticket = document.querySelector(".your-ticket");
-    ticket.className += " show";
+    total = parseFloat(total.toFixed(2));
+    sconto20 = parseFloat(sconto20.toFixed(2));
+    sconto40 = parseFloat(sconto40.toFixed(2));
+
+    document.querySelector(".pass-name").innerHTML = nameSurname;
+    document.querySelector(".carrozza").innerHTML = Math.floor(Math.random() * (10 - 1 + 1) ) + 1;
+    
+
+    ticket.classList.add("show");
 });
 
-
-let btnAnnulla = document.querySelector("cancel");
-
 btnAnnulla.addEventListener("click", function() {
-
-    let ticket = document.querySelector(".your-ticket");
-    ticket.className = "";
+    ticket.classList.remove("show");
 });
